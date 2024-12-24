3f74726f011ed1c1a666d1e864a83c797d6d6786
avisekh3110 <avisekh.sabi31102003@gmail.com>
2024-12-24 19:18:51 +0530

    {"M":["RecentActivityBArr"]}

--- src/components/recent.tsx
+++ src/components/recent.tsx
@@ -21,19 +21,29 @@ export default function Recent() {
 
   return (
     <div
-      className="flex justify-around items-center select-none rounded-lg h-40 sm:mx-10 lg:mx-36 
-      duration-300 bg-[url('/casecadeImg.png')] ease-out"
+      className="flex justify-around items-center select-none rounded-lg h-36 sm:mx-10 lg:mx-36 
+      duration-300 bg-[url('/casecadeImg.png')] ease-out p-4"
     >
-      <img
-        src={myPicture}
-        width={100}
-        className="h-24 w-24 rounded-full ring-4 ring-white "
-        title="Avisekh Sabi"
-      />
+      <div className="flex justify-center items-center py-2 px-4 gap-4">
+        <img
+          src={myPicture}
+          width={100}
+          className="h-24 w-24 rounded-full ring-4 ring-quarternary-a "
+          title="Avisekh Sabi"
+        />
+        <div className="hidden lg:flex flex-col">
+          <div className="text-quarternary-a font-mono font-bold text-xl">
+            Avisekh Sabi
+          </div>
+          <div className="text-quarternary-b font-mono font-bold text-base">
+            @avisekh_sabi
+          </div>
+        </div>
+      </div>
 
-      <div className="flex text-2xl sm:2xl font-bold font-sourGummy text-quarternary-a">
+      <div className="flex text-2xl sm:2xl font-bold font-mono text-quarternary-a">
         <div onClick={handleClick}>Currently working on</div>
-        <Link target="blank" to={""} className="pl-2">
+        <Link target="blank" to={""} className="pl-3 animate-pulse">
           {recent}
         </Link>
       </div>
@@ -42,7 +52,7 @@ export default function Recent() {
         to={
           "https://www.cloudskillsboost.google/public_profiles/eada8372-c786-4254-907c-aae29d5e0f43"
         }
-        className="bg-white p-4 rounded-sm flex justify-center items-center gap-5"
+        className="bg-quarternary-a p-4 rounded-sm flex justify-center items-center gap-5"
       >
         <img src={googleCloudLogo} width={30} />
         <div className="text-lg font-semibold hidden  lg:flex pointer-events-">
