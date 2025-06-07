965163138205b9abc6fa529ef86442a2a32ccbb6
avisekh3110 <avisekh.sabi31102003@gmail.com>
2025-06-08 00:52:24 +0530

    {C : ['todoturu.png', 'trackoexpense.png', 'trackotiny.png', 'projectPreview.tsx'], M : ['works.tsx'], D : []

---
+++ src/components/projectPreview.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+export default function ProjectPreview(prop: any) {
+  return (
+    <div className="bg-black p-4 flex flex-col gap-6 font-mono">
+      <div className="flex flex-col ">
+        <div className="text-2xl text-white">{prop.name}</div>
+        <div className="text-base text-slate-400 font-normal">
+          {prop.duration}
+        </div>
+      </div>
+      <div>{prop.description}</div>
+      <div className=" h-full rounded-md">
+        <img
+          src={prop.image}
+          alt="trackotiny img"
+          className="rounded-md ring-1 ring-slate-700"
+        />
+      </div>
+      <div className="flex gap-2">
+        {prop.techstack.map((element: string) => {
+          return (
+            <div className="font-thin text-sm ring-1 ring-slate-600 rounded-md px-4 py-1 text-slate-200">
+              {element}
+            </div>
+          );
+        })}
+      </div>
+      <Link
+        to={prop.link}
+        target="_blank"
+        className="bg-secondary rounded-md flex w-fit py-1 px-6 text-lg hover:bg-primary ring-1 ring-secondary duration-300 ease-out"
+      >
+        Try it out →
+      </Link>
+      <hr className="w-full mt-4" />
+    </div>
+  );
+}
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
+import ProjectPreview from "../components/projectPreview";
+import trackotiny from "/trackotiny.png";
+import trackoexpense from "/trackoexpense.png";
+import todoturu from "/todoturu.png";
 
 export default function Works() {
   type Exp = {
@@ -25,7 +29,7 @@ export default function Works() {
       position: "Android Developer",
       duration: "2021-2023",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, mollitia facere. Laudantium quod fuga dolores expedita, a odio explicabo, corporis cupiditate modi voluptates, incidunt quas ipsum iste impedit doloribus labore.",
+        "em ipsum dolor sit amet, consectetur adipisicin explicabo, corporis cupiditate modi voluptates, incidunt quas ipsum iste impeditLorg elit. Incidunt, mollitia facere. Laudantium quod fuga dolores expedita, a odio doloribus labore.",
     },
     {
       no: 3,
@@ -33,16 +37,56 @@ export default function Works() {
       position: "Network Engineer",
       duration: "2023-Present",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, mollitia facere. Laudantium quod fuga dolores expedita, a odio explicabo, corporis cupiditate modi voluptates, incidunt quas ipsum iste impedit doloribus labore.",
+        "lit. Incidunt, mollitia facere. Laudantium quod fuga dolores expedita, a odio explicLorem ipsum dolor sit amet, consectetur adipisicing eabo, corporis cupiditate modi voluptates, incidunt quas ipsum iste impedit doloribus labore.",
     },
   ];
+
+  type Proj = {
+    name: String;
+    duration: String;
+    description: String;
+    techstack: String[];
+    link: string;
+    image: string;
+  };
+
+  const projObj: Proj[] = [
+    {
+      name: "TrackOtiny",
+      duration: "2023-2024",
+      description:
+        "This is website which will be integrated with IOT to track location of GPS module controlled by ESP32 dev module. This project was assigned by CHANDIGARH UNIVERSITY in Independent Project under a topic of Child Tracking System. This uses vite js and tailwind in frontend and node js and express in backend (which further may get upgraded).",
+      techstack: ["react", "tailwindCSS", "react-map-gl", "vite"],
+      link: "https://trackotiny.vercel.app/",
+      image: trackotiny,
+    },
+    {
+      name: "TrackOExpense",
+      duration: "2025-Present",
+      description:
+        "TrackoExpense is a simple and efficient expense tracking application built with React and Vite. This project aims to help users manage their expenses by providing an intuitive interface and seamless user experience.",
+      techstack: ["react", "tailwindCSS", "recharts", "vite", "react-toastify"],
+      link: "https://trackoexpense.vercel.app/",
+      image: trackoexpense,
+    },
+    {
+      name: "Todo_turu",
+      duration: "2022-2023",
+      description:
+        "It is a Todo List web site which will help you jotting your daily work down. Enjoy!!",
+      techstack: ["react", "tailwindCSS", "express", "vite", "axios"],
+      link: "https://todo-turu.vercel.app/",
+      image: todoturu,
+    },
+  ];
+
   const [ActiveExp, setActiveExp] = useState<Exp>(expObj[0]);
   console.log(ActiveExp);
 
   return (
-    <div className=" bg-primary h-screen">
+    <div className=" bg-primary h-full">
       <Navbar />
-      <div className=" flex h-full justify-center text-white font-bold">
+      <div className=" flex h-full text-white font-bold flex-col items-center gap-10">
         <div className=" h-96 w-[44rem] mt-40 flex flex-col gap-4">
           <div className=" h-1/6 w-full flex justify-start items-center p-4 text-3xl">
             MY WORK EXPERIENCE
@@ -82,18 +126,28 @@ export default function Works() {
               </div>
             </div>
             <div className=" w-3/4 flex flex-col py-4">
-              <div className=" w-full h-1/4 flex flex-col">
-                <div className="flex  h-2/3 text-xl justify-start items-center px-4">
+              <div className=" w-full h-1/4 flex flex-col duration-500">
+                <div className="flex  h-2/3 text-xl justify-start items-center px-4 duration-500">
                   {ActiveExp.position} @{ActiveExp.name}
                 </div>
-                <div className="flex h-1/3 justify-start items-center px-4 text-slate-400 font-mono">
+                <div className="flex h-1/3 justify-start items-center px-4 text-slate-400 font-mono duration-500">
                   {ActiveExp.duration}
                 </div>
               </div>
-              <div className="h-3/4 w-full p-4 flex justify-start text-secondary">
+              <div className="h-3/4 w-full p-4 flex justify-start text-secondary duration-500">
                 {ActiveExp.description}
               </div>
             </div>
+          </div>
+        </div>
+        <div className="h-full w-[50rem]  flex flex-col gap-6">
+          <div className=" flex items-center justify-center text-3xl hover:tracking-widest duration-300">
+            P R O J E C T S
+          </div>
+          <div className="h-full w-full flex flex-col gap-4">
+            {projObj.map((element: any) => {
+              return ProjectPreview(element);
+            })}
           </div>
         </div>
       </div>
